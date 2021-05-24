8df41f6433584e61c2fb7948f54a25b19e988983
Stacey McKnight <mcknight.stm@gmail.com>
2021-05-24 17:16:54 -0500

    solution 1 to absolute difference challenge

--- hacker-rank/warm-up.js
+++ hacker-rank/warm-up.js
@@ -28,3 +28,37 @@ function compareTriplets(a, b) {
 function aVeryBigSum(ar) {
   return ar.reduce((memo, num) => BigInt(memo) + BigInt(num), 0)
 }
+
+//Given a square matrix, calculate the absolute difference between the sums of its diagonals
+//Attempt 1
+const two = [2, 4, -1, 2, 3]
+const three = [3, 11, 2, 4, 4, 5, 6, 10, 8, -12]
+const four = [4, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+
+function sum(arr) {
+  return arr.reduce((a, b) => {
+    return a + b;
+  });
+}
+
+function absDiffDiag(arr) {
+  const sq = arr[0]
+  let diagNums = []
+  let diagNums2 = []
+ 
+  let i = 1;
+  while (i < arr.length) {
+    diagNums.push(arr[i]);
+    i += (sq + 1);
+  }
+
+  let j = sq;
+  while (j <= (arr.length - sq)) {
+    diagNums2.push(arr[j]);
+    j += (sq - 1);
+  }
+  
+  return Math.abs(sum(diagNums) - sum(diagNums2))
+}
+
+
