21b15c68768c7818e8e81fe2bcbd3fd5f3ecb756
Stacey McKnight <mcknight.stm@gmail.com>
2021-09-07 17:01:57 -0500

    add case statement example

--- hacker-rank/ruby-review.rb
+++ hacker-rank/ruby-review.rb
@@ -4,3 +4,19 @@ def scoring(user_array)
     user.update_score unless user.is_admin?
   end
 end
+
+# Write a case statement that prints output based on object type passed in as argument
+def determine_class(obj)
+  case obj
+  when Hacker
+    puts "It's a Hacker!"
+  when Submission
+    puts "It's a Submission!"
+  when TestCase
+    puts "It's a TestCase!"
+  when Contest
+    puts "It's a Contest!"
+  else
+    puts "It's an unknown model"
+  end
+end
